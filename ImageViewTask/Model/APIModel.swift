eabaab72fb113943719cec1e799a5bc842feca15
Igor Polousov <igorpolousov@gmail.com>
2022-03-01 18:11:03 +0300

    - remove test structure - remove test images array

--- ImageViewTask/Model/APIModel.swift
+++ ImageViewTask/Model/APIModel.swift
@@ -49,18 +49,4 @@ func request(url: URL) {
     task.resume()
 }
 
- // Структура для проверки работы интерфейса
-struct ImageTalantix {
-    var date: String
-    var image: UIImage
-}
 
-// Массив картинок для проверки работы интерфейса
-var images = [
-    ImageTalantix(date: "22.02.2022", image: UIImage.init(imageLiteralResourceName: "nssl0033")),
-    ImageTalantix(date: "15.06.2019", image: UIImage.init(imageLiteralResourceName: "nssl0034")),
-    ImageTalantix(date: "17.11.2018", image: UIImage.init(imageLiteralResourceName: "nssl0041")),
-    ImageTalantix(date: "19.08.2021", image: UIImage.init(imageLiteralResourceName: "nssl0049")),
-    ImageTalantix(date: "09.09.1990", image: UIImage.init(imageLiteralResourceName: "nssl0051")),
-    ImageTalantix(date: "12.12.1998", image: UIImage.init(imageLiteralResourceName: "nssl0091"))
-]
