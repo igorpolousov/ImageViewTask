db5f11ea64f808dc5012dc654fbb386625b422d9
Igor Polousov <igorpolousov@gmail.com>
2022-03-01 14:53:02 +0300

    Initial Commit

---
+++ ImageViewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageViewTask
+//
+//  Created by Igor Polousov on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
